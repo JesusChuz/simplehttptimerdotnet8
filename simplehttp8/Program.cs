35bcebe81039cd8f8e7cc29c8cde6ee0c8f98799
unknown <j.mendezquesada18@gmail.com>
2024-06-12 10:52:33 -0600

    add code fix for isolateds

--- simplehttp8/Program.cs
+++ simplehttp8/Program.cs
@@ -1,13 +1,24 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWebApplication()
+    .ConfigureFunctionsWorkerDefaults()
     .ConfigureServices(services =>
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+    }).ConfigureLogging(log =>
+    {
+        log.Services.Configure<LoggerFilterOptions>(opt =>
+        {
+            LoggerFilterRule newRule = opt.Rules.FirstOrDefault(rule => rule.ProviderName == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+            if (newRule is not null)
+            {
+                opt.Rules.Remove(newRule);
+            }
+        });
     })
     .Build();
 
